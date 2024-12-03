ca1cdbc11108174216a1f049459bf5b1dc5d030c
student250 <student250@250-4>
2024-12-03 09:19:25 +0300

    CLOSE #1 Создали фому для импорта данных и восстановления БД

--- Optics/ImportDataForm.Designer.cs
+++ Optics/ImportDataForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Optics
 {
-    partial class ReportForm
+    partial class ImportDataForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,12 @@ namespace Optics
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportDataForm));
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
@@ -43,7 +44,7 @@ namespace Optics
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Sitka Banner", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.button2.Location = new System.Drawing.Point(17, 132);
+            this.button2.Location = new System.Drawing.Point(17, 221);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(265, 44);
             this.button2.TabIndex = 11;
@@ -55,9 +56,9 @@ namespace Optics
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 28);
+            this.label1.Size = new System.Drawing.Size(89, 28);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Тип отчета";
+            this.label1.Text = "Таблица";
             // 
             // comboBox1
             // 
@@ -75,20 +76,35 @@ namespace Optics
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Sitka Banner", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button3.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.button3.Location = new System.Drawing.Point(17, 82);
+            this.button3.Location = new System.Drawing.Point(17, 171);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(265, 44);
             this.button3.TabIndex = 70;
-            this.button3.Text = "Сформировать отчет";
+            this.button3.Text = "Восстановить БД";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // ReportForm
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.GrayText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Sitka Banner", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(17, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 44);
+            this.button1.TabIndex = 71;
+            this.button1.Text = "Импортировать";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // ImportDataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(301, 188);
+            this.ClientSize = new System.Drawing.Size(301, 284);
             this.ControlBox = false;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
@@ -99,9 +115,9 @@ namespace Optics
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ReportForm";
+            this.Name = "ImportDataForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Оптика Визион - Отчеты";
+            this.Text = "Оптика Визион - Импорт и восстановление";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +128,7 @@ namespace Optics
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
